6d4155771f88efd31a0c2a5d48ec6cb2df635299
Austin Klopfer <auklopfe@microsoft.com>
2022-01-17 13:37:53 -0800

    add tons or useless logs for testing

---
+++ readme.txt
@@ -0,0 +1,6 @@
+code from sample app tutorial: https://www.digitalocean.com/community/tutorials/build-a-to-do-application-using-django-and-react
+
+to run:
+-pipenv shell
+-(if not already installed)pipenv install django
+-python manage.py runserver
--- todo/views.py
+++ todo/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
+import threading
+import logging
+import time
 
 # Create your views here.
+def endlessLogs():
+    while True:
+        logging.warning('Watch out! here comes a flood of logs')
+        print("print log")
+        time.sleep(10)
+
+
+
 
 class TodoView(viewsets.ModelViewSet):
+    t = threading.Thread(target=endlessLogs)
+    t.setDaemon(True)
+    t.start()
+    #above is just a thread to write logs
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
