c10f06c8f57a49e719edd1e375f7b73ec219f432
Austin Klopfer <auklopfe@microsoft.com>
2022-01-19 08:32:11 -0800

    sleep a bit to not stress system

--- todo/views.py
+++ todo/views.py
@@ -7,13 +7,14 @@ import logging
 import time
 
 # Create your views here.
+# Austin perhaps have a huge log message either ssaved or constructed that's constant and only emit logs every 1 second so I know how many logs to expect exactly so I can see if were missing any
 def endlessLogs():
     while True:
         logging.warning('Watch out! here comes a flood of logs')
         logging.info('ERROR hello hello')
         logging.error('wow wow ')
         print("print log1")
-
+        time.sleep(15)
 
 
 
