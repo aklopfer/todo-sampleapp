07e6c00861aae379baf2b29210281748e44c0b1f
Austin Klopfer <auklopfe@microsoft.com>
2022-01-19 20:41:51 -0800

    add numbering

--- todo/views.py
+++ todo/views.py
@@ -6,15 +6,18 @@ import threading
 import logging
 import time
 
+x = 0
 # Create your views here.
 # Austin perhaps have a huge log message either ssaved or constructed that's constant and only emit logs every 1 second so I know how many logs to expect exactly so I can see if were missing any
 def endlessLogs():
     while True:
         logging.warning('Watch out! here comes a flood of logs')
         logging.info('ERROR hello hello')
-        logging.error('wow wow ')
+        global x
+        logging.error('lognumber:'+str(x))
+        x+=1
         print("print log1")
-        time.sleep(15)
+        time.sleep(.003)
 
 
 
